41fb8e5620b1c952530a7671ebd51e6ec24da31d
Marko Sanković <sankovic.marko@gmail.com>
2014-11-02 18:35:30 +0100

    Update state message

--- src/main/java/com/synapticon/buckeusbaccessory/FXMLController.java
+++ src/main/java/com/synapticon/buckeusbaccessory/FXMLController.java
@@ -2,6 +2,7 @@ package com.synapticon.buckeusbaccessory;
 
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 import java.util.logging.Handler;
@@ -9,8 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,11 +17,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 import javax.usb.UsbException;
 import javax.usb.event.UsbDeviceDataEvent;
@@ -44,18 +40,16 @@ public class FXMLController implements Initializable {
             "http://bucke.synapticon.com/",
             "0");
 
-    // static final short DEVICE_VENDOR_ID = (short) 0x18D1; // Google
-    // static final short DEVICE_PRODUCT_ID = (short) 0xD002; // Google Nexus 4
     static final short DEVICE_VENDOR_ID = (short) 0x0bb4; // HTC Corporation
     static final short DEVICE_PRODUCT_ID = (short) 0x0f63; // HTC One mini
 
     static final short ACCESSORY_VENDOR_ID = (short) 0x18D1;
     static final short ACCESSORY_PRODUCT_ID = (short) 0x2D01;
 
-    Thread readThread;
-    Thread drivingModeThread;
-
     byte states = 0x00;
+
+    Thread readThread;
+    Thread stateMessageThread;
 
     UsbDeviceListener usbDeviceListener = new UsbDeviceListener() {
         @Override
@@ -65,7 +59,7 @@ public class FXMLController implements Initializable {
             Platform.runLater(new Runnable() {
                 @Override
                 public void run() {
-                    connectButton.setDisable(false);
+                    switchToUSBAccessoryModeButton.setDisable(false);
                 }
             });
 
@@ -74,6 +68,7 @@ public class FXMLController implements Initializable {
 
         @Override
         public void errorEventOccurred(UsbDeviceErrorEvent udee) {
+            logger.log(Level.INFO, udee.getUsbException().getMessage());
         }
 
         @Override
@@ -86,24 +81,7 @@ public class FXMLController implements Initializable {
     String code;
 
     @FXML
-    Slider speedSlider;
-    byte speed = 0x05;
-
-    @FXML
-    Slider recuperationStateSlider;
-    short recuperationState = 0x00;
-
-    @FXML
-    Slider remainingDistanceSlider;
-    short remainingDistance = 0x00;
-
-    @FXML
-    Slider powerConsumptionSlider;
-    short powerConsumption = 0x00;
-
-    @FXML
-    ComboBox sendingIntervalComboBox;
-    int sendingInterval;
+    ComboBox sendIntervalComboBox;
 
     @FXML
     void handleCodeTextChanged(ActionEvent event) {
@@ -117,67 +95,12 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        // Try to connect to Android device on initialize
-        handleConnect();
-
         // Set initial code
         code = codeTextField.getText();
 
-        // Speed Slider
-        speedSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                byte newSpeed = newValue.byteValue();
-                if (speed != newSpeed) {
-                    speed = newSpeed;
-                    logger.log(Level.INFO, String.format("New speed value: %s", String.valueOf(speed)));
-                }
-            }
-        });
-
-        // Recuperation State Slider
-        recuperationStateSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                short newRecuperationState = newValue.shortValue();
-                if (recuperationState != newRecuperationState) {
-                    recuperationState = newRecuperationState;
-                    logger.log(Level.INFO, String.format("New recuperation state value: %s", String.valueOf(recuperationState)));
-                }
-            }
-        });
-
-        // Remaining Distance Slider
-        remainingDistanceSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                short newRemainingDistance = newValue.shortValue();
-                if (remainingDistance != newRemainingDistance) {
-                    remainingDistance = newRemainingDistance;
-                    logger.log(Level.INFO, String.format("New remaining distance value: %s", String.valueOf(remainingDistance)));
-                }
-            }
-        });
-
-        // Power Consumption Slider
-        powerConsumptionSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                short newPowerConsumption = newValue.shortValue();
-                if (powerConsumption != newPowerConsumption) {
-                    powerConsumption = newPowerConsumption;
-                    logger.log(Level.INFO, String.format("New power consumption value: %s", String.valueOf(powerConsumption)));
-                }
-            }
-        });
-
-        // Sending Interval
-        ObservableList<Integer> intervals = FXCollections.observableArrayList(10, 50, 100, 200, 500, 1000, 3000);
-        sendingIntervalComboBox.setItems(intervals);
+        // Send Interval
+        ObservableList<Integer> intervals = FXCollections.observableArrayList(5, 10, 15, 20, 50, 100, 200, 500, 1000, 3000);
+        sendIntervalComboBox.setItems(intervals);
 
         // Setup logger handler (output logs to TextArea)
         logger.addHandler(new Handler() {
@@ -219,64 +142,20 @@ public class FXMLController implements Initializable {
     }
 
     @FXML
-    MenuItem closeMenuItem;
+    Button switchToUSBAccessoryModeButton;
 
     @FXML
-    Button connectButton;
-
-    @FXML
-    ToggleButton modeToggleButton;
-
-    @FXML
-    ToggleButton simulateDrivingToggle;
-
-    @FXML
-    void handleClose() {
+    void onClose() {
         logger.log(Level.INFO, "Handle Close");
-        Stage stage = (Stage) connectButton.getScene().getWindow();
+        Stage stage = (Stage) switchToUSBAccessoryModeButton.getScene().getWindow();
         closeAndroidDevice();
         stage.close();
-    }
-
-    @FXML
-    void handleBoostToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x01);
-        logger.log(Level.INFO, String.format("States: %d", states));
-    }
-
-    @FXML
-    void handleOverheatingToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x02);
-        logger.log(Level.INFO, String.format("States: %d", states));
-    }
-
-    @FXML
-    void handleLowBeamToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x04);
-        logger.log(Level.INFO, String.format("States: %d", states));
-    }
-
-    @FXML
-    void handleHighBeamToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x08);
-        logger.log(Level.INFO, String.format("States: %d", states));
-    }
-
-    @FXML
-    void handleLeftTurnSignalToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x10);
-        logger.log(Level.INFO, String.format("States: %d", states));
-    }
-
-    @FXML
-    void handleRightTurnSignalToggle(ActionEvent event) {
-        states = (byte) (states ^ 0x20);
-        logger.log(Level.INFO, String.format("States: %d", states));
     }
 
     void closeAndroidDevice() {
         if (androidDevice != null) {
             try {
+                androidDevice.sendMessage(OnBoardControllerConstants.SOFT_CLOSE_MESSAGE, new byte[]{});
                 androidDevice.close();
             } catch (UsbException ex) {
                 logger.log(Level.SEVERE, null, ex);
@@ -285,9 +164,44 @@ public class FXMLController implements Initializable {
     }
 
     @FXML
-    void handleConnect() {
-        try {
+    void onSmartphoneConnected(ActionEvent event) {
+        states = (byte) (states ^ 0x01);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
 
+    @FXML
+    void onHeadlightToggle(ActionEvent event) {
+        states = (byte) (states ^ 0x02);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
+
+    @FXML
+    void onCameraToggle(ActionEvent event) {
+        states = (byte) (states ^ 0x04);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
+
+    @FXML
+    void onLeftTurnSignalToggle(ActionEvent event) {
+        states = (byte) (states ^ 0x08);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
+
+    @FXML
+    void onRightTurnSignalToggle(ActionEvent event) {
+        states = (byte) (states ^ 0x10);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
+
+    @FXML
+    void onGasThrottleIdleStateToggle(ActionEvent event) {
+        states = (byte) (states ^ 0x20);
+        logger.log(Level.INFO, String.format("States: %d", states));
+    }
+
+    @FXML
+    void onSwitchToUSBAccessoryMode() {
+        try {
             // Get Android device (switch Android device to USB accessory mode)
             androidDevice = new AndroidDevice(new AndroidOpenAccessory(
                     identifyingInformation,
@@ -301,32 +215,17 @@ public class FXMLController implements Initializable {
             readThread = new Thread(new ReadRunnable());
             readThread.start();
 
+            // Start new state message thread
+            stateMessageThread = new Thread(new StateMessageRunnable());
+            stateMessageThread.start();
+            
             // Disable connect button
-            connectButton.setDisable(true);
+            switchToUSBAccessoryModeButton.setDisable(true);
 
             logger.log(Level.INFO, "Connected to USB device");
-
         } catch (UsbException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
-    }
-
-    @FXML
-    void handleModeChange(ActionEvent event) {
-        if (modeToggleButton.isSelected()) {
-            modeToggleButton.setText("DRIVING MODE");
-            modeToggleButton.setDisable(false);
-        } else {
-            modeToggleButton.setText("STANDSTILL");
-            modeToggleButton.setDisable(true);
-            drivingModeThread.interrupt();
-            drivingModeThread = null;
-        }
-    }
-
-    @FXML
-    void handleSendSmartphoneConnected(ActionEvent event) {
-        logger.log(Level.INFO, "Handle Send Smartphone Connected");
     }
 
     class ReadRunnable implements Runnable {
@@ -342,9 +241,9 @@ public class FXMLController implements Initializable {
                     short command = Utils.twoBytesToShort(data[0], data[1]);
 
                     switch (command) {
-                        case OnBoardControllerConstants.REQUEST_CLOSE_COMMAND:
+                        case OnBoardControllerConstants.SOFT_CLOSE_COMMAND:
                             logger.log(Level.INFO, "Close request initiated. Send 0x19 to the attached device.");
-                            androidDevice.sendMessage(OnBoardControllerConstants.CLOSE_MESSAGE, new byte[]{});
+                            androidDevice.sendMessage(OnBoardControllerConstants.SOFT_CLOSE_MESSAGE, new byte[]{});
                             break;
                         case OnBoardControllerConstants.VERIFY_CODE_COMMAND:
                             if (received > 2) {
@@ -364,22 +263,6 @@ public class FXMLController implements Initializable {
                                 androidDevice.sendMessage(OnBoardControllerConstants.CODE_VERIFICATION_MESSAGE, new byte[]{2});
                             }
                             break;
-                        case OnBoardControllerConstants.SWITCH_TO_DRIVING_MODE_COMMAND:
-                            logger.log(Level.INFO, "Switch to driving mode request.");
-
-                            drivingModeThread = new Thread(new DrivingModeRunnable());
-                            drivingModeThread.start();
-
-                            Platform.runLater(new Runnable() {
-                                @Override
-                                public void run() {
-                                    modeToggleButton.setText("DRIVING MODE");
-                                    modeToggleButton.setDisable(false);
-                                    modeToggleButton.setSelected(true);
-                                }
-                            });
-
-                            break;
                         default:
                             logger.log(Level.WARNING, String.format("No such command: %d", data[0]));
                     }
@@ -394,81 +277,42 @@ public class FXMLController implements Initializable {
         }
     }
 
-    class DrivingModeRunnable implements Runnable {
+    class StateMessageRunnable implements Runnable {
 
         @Override
         public void run() {
             while (true) {
                 try {
-                    ByteBuffer buffer = ByteBuffer.allocate(8);
-
+                    ByteBuffer buffer = ByteBuffer.allocate(10);
+                    buffer.order(ByteOrder.LITTLE_ENDIAN);
                     buffer.put(states);
-                    buffer.put(speed);
-                    buffer.putShort(recuperationState);
-                    buffer.putShort(remainingDistance);
-                    buffer.putShort(powerConsumption);
-
+                    buffer.putShort((short) 64); // SPEED
+                    buffer.putShort((short) -128); // BATTERY_POWER
+                    buffer.put((byte) 0x4D); // BATTERY_STATE_OF_CHARGE
+                    buffer.put((byte) 0xFF); // REMAINING_DISTANCE
+                    buffer.putShort((short) 48347);
+                    buffer.put((byte) 0x20);
+                    
                     androidDevice.sendMessage(OnBoardControllerConstants.OBC_STATE_MESSAGE, buffer.array());
-
-                    Integer interval = (Integer) sendingIntervalComboBox.getSelectionModel().getSelectedItem();
-
+                    
+                    Integer interval = (Integer) sendIntervalComboBox.getSelectionModel().getSelectedItem();
+                    
                     if (interval == null) {
-                        interval = (Integer) sendingIntervalComboBox.getItems().get(1);
+                        interval = (Integer) sendIntervalComboBox.getItems().get(1);
                         Platform.runLater(new Runnable() {
                             @Override
                             public void run() {
-                                sendingIntervalComboBox.setValue(sendingIntervalComboBox.getItems().get(1));
+                                sendIntervalComboBox.setValue(sendIntervalComboBox.getItems().get(1));
                             }
                         });
                     }
-
+                    
                     Thread.sleep(interval);
-
-                } catch (InterruptedException ex) {
+                } catch (InterruptedException e) {
                     logger.log(Level.INFO, "Driving mode thread is interrupted.");
                     break;
                 }
-            }
+           }
         }
     }
-
-    class SimulateDrivingModeRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-
-                    ByteBuffer buffer = ByteBuffer.allocate(8);
-
-                    buffer.put(states);
-                    buffer.put(speed);
-                    buffer.putShort(recuperationState);
-                    buffer.putShort(remainingDistance);
-                    buffer.putShort(powerConsumption);
-
-                    androidDevice.sendMessage(OnBoardControllerConstants.OBC_STATE_MESSAGE, buffer.array());
-
-                    Integer interval = (Integer) sendingIntervalComboBox.getSelectionModel().getSelectedItem();
-
-                    if (interval == null) {
-                        interval = (Integer) sendingIntervalComboBox.getItems().get(1);
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                sendingIntervalComboBox.setValue(sendingIntervalComboBox.getItems().get(1));
-                            }
-                        });
-                    }
-
-                    Thread.sleep(interval);
-
-                } catch (InterruptedException ex) {
-                    logger.log(Level.INFO, "Driving mode thread is interrupted.");
-                    break;
-                }
-            }
-        }
-    }
-
 }
--- src/main/java/com/synapticon/buckeusbaccessory/OnBoardControllerConstants.java
+++ src/main/java/com/synapticon/buckeusbaccessory/OnBoardControllerConstants.java
@@ -9,15 +9,18 @@ public class OnBoardControllerConstants {
     // Commands
 
     public static final short NOP_COMMAND = 0x00;
-    public static final short REQUEST_CLOSE_COMMAND = 0x41;
+    public static final short SOFT_CLOSE_COMMAND = 0x41;
     public static final short VERIFY_CODE_COMMAND = 0x42;
-    public static final short SWITCH_TO_DRIVING_MODE_COMMAND = 0x43;
+    public static final short RELEASE_DRIVETRAIN_COMMAND = 0x43;
+    public static final short DISABLE_DRIVETRAIN_COMMAND = 0x44;
+
 
     // Messages
 
     public static final short OBC_STATE_MESSAGE = 0x01;
-    public static final short CLOSE_MESSAGE = 0x41;
+    public static final short SOFT_CLOSE_MESSAGE = 0x41;
     public static final short CODE_VERIFICATION_MESSAGE = 0x42;
-    public static final short SWITCH_TO_DRIVING_MODE_MESSAGE = 0x43;
+    public static final short RELEASE_DRIVETRAIN_MESSAGE = 0x43;
+    public static final short DISABLE_DRIVETRAIN_MESSAGE = 0x44;
 
 }
