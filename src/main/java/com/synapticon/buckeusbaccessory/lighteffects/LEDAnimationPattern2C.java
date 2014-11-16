d2323cb10cd152850ca725658aa91deb44e2daa8
Marko Sanković <sankovic.marko@gmail.com>
2014-11-16 22:48:40 +0100

    Create and animate LEDAnimationPattern2C

--- src/main/java/com/synapticon/buckeusbaccessory/lighteffects/LEDAnimationPattern2C.java
+++ src/main/java/com/synapticon/buckeusbaccessory/lighteffects/LEDAnimationPattern2C.java
@@ -1,5 +1,7 @@
 package com.synapticon.buckeusbaccessory.lighteffects;
 
+import java.util.Random;
+
 /**
  * LIGHT EFFECTS - PATTERN 2 / LED Animation C
  *
@@ -8,15 +10,62 @@ package com.synapticon.buckeusbaccessory.lighteffects;
  */
 public class LEDAnimationPattern2C extends LEDAnimation {
 
+    private final Random random;
+    
+    private boolean frontFlash = true;
+
+    private int rearFlashGroup = 0;
+
     public LEDAnimationPattern2C(LEDUpdater ledUpdater, int duration) {
         super(ledUpdater, duration, 1000, 1000);
-    }
-
-    @Override
-    protected void animateFrontLED() {
+        
+        random = new Random();
     }
 
     @Override
     protected void animateRearLED() {
+        byte[] bytes = new byte[getRearLEDBytes().length];
+
+        byte r = (byte) random.nextInt(255);
+        byte g = (byte) random.nextInt(255);
+        byte b = (byte) random.nextInt(255);
+        
+        if (rearFlashGroup == 0) {
+            for (int i = 0; i < 51; i += 3) {
+                bytes[i] = r;
+                bytes[i + 1] = g;
+                bytes[i + 2] = b;
+            }
+        } else if (rearFlashGroup == 1) {
+            for (int i = 51; i < 108; i += 3) {
+                bytes[i] = r;
+                bytes[i + 1] = g;
+                bytes[i + 2] = b;
+            }
+        } else if (rearFlashGroup == 2) {
+            for (int i = 108; i < 159; i += 3) {
+                bytes[i] = r;
+                bytes[i + 1] = g;
+                bytes[i + 2] = b;
+            }
+        }
+
+        rearFlashGroup = rearFlashGroup == 2 ? 0 : rearFlashGroup + 1;
+
+        setRearLEDBytes(bytes);
+    }
+
+    @Override
+    protected void animateFrontLED() {
+        byte[] bytes = new byte[getFrontLEDBytes().length];
+        if (frontFlash) {
+            for (int i = 0; i < getFrontLEDBytes().length; i += 3) {
+                bytes[i] = (byte) 36;
+                bytes[i + 1] = (byte) 148;
+                bytes[i + 2] = (byte) 253;
+            }
+        }
+        frontFlash = !frontFlash;
+        setFrontLEDBytes(bytes);
     }
 }
